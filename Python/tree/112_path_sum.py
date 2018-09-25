928f7dea94595a99663d61fd34eca8d5c33e8943
Jacky <hklee310@gmail.com>
2018-09-25 14:09:55 -0700

    112 path sum DFS and recursive solution

--- Python/tree/112_path_sum.py
+++ Python/tree/112_path_sum.py
@@ -9,16 +9,41 @@
 
 class Solution(object):
     def hasPathSum(self, root, sum):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: bool
-        """
+        #return self.hasPathSum_recursive(root, sum)
+        return self.hasPathSum_DFS(root, sum)
+
+    def hasPathSum_recursive(self, root, sum):
+        # base cases
         if root is None:
             return False
-        if root.left is None and root.right is None:    # Leaf Node
-            return 0 == (sum - root.val)
+        if root.left is None and root.right is None:  # if leafnode and sum is down to 0 we fond the path
+            return (sum - root.val) == 0
 
-        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
-    
+        # else not at leaf node
+        return self.hasPathSum_recursive(root.left, sum - root.val) or \
+               self.hasPathSum_recursive(root.right, sum - root.val)
 
+    def hasPathSum_DFS(selfself, root, sum):
+        if root is None:
+            return False
+
+        # DFS using a stack to hold tuple of (node, remaining sum)
+        search_stack = [(root, sum)]
+
+        while search_stack:
+            popped     = search_stack.pop()
+            node       = popped[0]
+            sum_remain = popped[1] - node.val
+
+            # Goal check
+            if node.left is None and node.right is None:    # if leaf node
+                if sum_remain == 0:
+                    return True
+
+            # Continue DFS
+            if node.left:
+                search_stack.append((node.left, sum_remain))
+            if node.right:
+                search_stack.append((node.right, sum_remain))
+
+        return False
